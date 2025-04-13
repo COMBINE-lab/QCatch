3eabc8c42fbe74773a5068c86d72abb367c720bd
ygao61 <ygao61@umd.edu>
2025-04-13 14:50:47 -0400

    Release v 0.2.1  Fix: updaete v number; add new dependency version for env.yml and pyproject.toml

--- release_PyPI.sh
+++ release_PyPI.sh
@@ -3,3 +3,10 @@ git commit -m "Release qcatch v0.2.1"
 # git tag v0.2.1          
 git push origin main    
 # git push origin v0.2.1  
+
+
+# Build the package locally
+python -m build
+
+# Upload the package to PyPI
+twine upload dist/*
