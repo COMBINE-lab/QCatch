3f1b147a8d2995ada8e4c2ddbf37a621aec29379
ygao61 <ygao61@umd.edu>
2025-05-12 14:01:34 -0400

    Refactor project structure using cookiecutter template

--- release_PyPI.sh
+++ release_PyPI.sh
@@ -1,8 +1,8 @@
 git add .
-git commit -m "release v0.2.3" # need to change each time
-# git tag v0.2.1          
-git push origin main    
-# git push origin v0.2.1  
+git commit -m "release v0.2.4" # need to change each time
+git tag v0.2.4
+git push origin main
+# git push origin v0.2.1
 
 
 # Build the package locally
